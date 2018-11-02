fdd8964c79549739596f60bfd3b5d7c246c1b59d
Nguyen Phuc Tu <phuctu1901@Nguyens-iMac.local>
2018-11-02 21:06:55 +0700

    remove trick

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,6 +1,4 @@
-import Web3 from 'web3'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 // const mnemonic = 'pole noise auction waste favorite shield gallery initial elite kitchen physical always'; // 12 word mnemonic
 const mnemonic = 'matrix sample more cool danger strong number effort outdoor all amateur insect'
 var HDWalletProvider = require('truffle-hdwallet-provider')
@@ -11,36 +9,43 @@ console.log(parsed.plate_id);
 var plate_id = parsed.plate_id;
 //=> {foo: 'bar'}
 var customProvider = new HDWalletProvider(mnemonic, 'https://testnet.tomochain.com');
-web3 = new Web3(customProvider);
+var web3 = new Web3(customProvider);
 console.log(customProvider);
 
-var abi = [{
+var abi = [
+	{
 		"constant": false,
-		"inputs": [{
-			"name": "_plate_id",
-			"type": "string"
-		}],
+		"inputs": [
+			{
+				"name": "_plate_id",
+				"type": "string"
+			}
+		],
 		"name": "verifyVehicleatBot",
-		"outputs": [{
-			"name": "",
-			"type": "bool"
-		}],
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"inputs": [{
-			"name": "_t",
-			"type": "address"
-		}],
+		"inputs": [
+			{
+				"name": "_t",
+				"type": "address"
+			}
+		],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "constructor"
 	}
 ];
 
-var contractAddress = '0xe0b253ca40d39da601d83263e49852ed65d6f4bd';
+var contractAddress = '0xfde80ce1bf3d2a78f4797a1fc8328f824f68dc56';
 var BotWorkStationABI = web3.eth.contract(abi);
 var BotWorkStationContract = BotWorkStationABI.at(contractAddress);
 
@@ -49,6 +54,7 @@ var BotWorkStationContract = BotWorkStationABI.at(contractAddress);
 var getData = BotWorkStationContract.verifyVehicleatBot.getData(plate_id);
 
 console.log(getData);
+
 web3.eth.sendTransaction({
 	to: contractAddress,
 	from: "0x94bbf0f6d92cffd0bf11a79f4106da3dfc159207",
